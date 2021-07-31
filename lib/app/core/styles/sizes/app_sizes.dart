d8ac60fb0eb24223ce65f478e8ee2bcfdf0f8223
Douglas Wender <dglswender@gmail.com>
2021-07-31 17:22:35 -0300

    AppSizes

---
+++ lib/app/core/styles/sizes/app_sizes.dart
@@ -0,0 +1,8 @@
+class AppSizes {
+  static final double s8 = 8;
+  static final double s16 = 16;
+  static final double s24 = 24;
+  static final double s32 = 32;
+  static final double s64 = 64;
+  static final double s128 = 128;
+}
